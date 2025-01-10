35cc8ffe3d4d1e67880eba32a508dd4df68cbb40
Jason Swann <jpswann@Jasons-MacBook-Pro.local>
2025-01-10 15:42:10 -0500

    handle user session

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
-  get "about", to: 'pages#about'
+  get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   get "up" => "rails/health#show", as: :rails_health_check
 end
