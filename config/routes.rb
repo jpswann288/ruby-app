4e40d1b35a6f35c792b8e66d6049df6d67918606
Jason Swann <jpswann@Jasons-MacBook-Pro.local>
2025-01-10 12:34:27 -0500

    add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
   get "about", to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   get "up" => "rails/health#show", as: :rails_health_check
 end
